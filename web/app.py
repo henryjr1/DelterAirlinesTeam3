eafb7d7d3480f8793266797e536637e4479eb7d7
aqd14 <aqd14@msstate.edu>
2017-11-21 20:52:15 -0600

    workaround to force postgres server to run

--- web/app.py
+++ web/app.py
@@ -2,15 +2,25 @@
 from flask_restful import Api
 from app import app
 
-from app.views import FlightAPI, FlightSearchAPI, TicketAPI, FlightListAPI
+from app.views import FlightAPI, FlightSearchAPI, TicketAPI, FlightListAPI, OrderAPI
 from instance.db_create import init_db
+import time
 
 api = Api(app, prefix="/api/v1.0")
 api.add_resource(FlightListAPI, '/flights')
 api.add_resource(FlightAPI, '/flights/<string:flight_id>')
 api.add_resource(FlightSearchAPI, '/Flight-Search')
 api.add_resource(TicketAPI, '/tickets')
+api.add_resource(OrderAPI, '/order')
 
 if __name__ == '__main__':
-    init_db()
+    dbstatus = False
+    while dbstatus == False:
+        try:
+            init_db()
+        except:
+            time.sleep(2)
+        else:
+            dbstatus = True
+
     app.run(debug=True, host="0.0.0.0")
