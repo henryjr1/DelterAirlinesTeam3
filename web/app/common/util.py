4848b1a7b4aa079dd0e626d0ce3ec1ca689e227c
aqd14 <aqd14@msstate.edu>
2017-11-29 12:13:02 -0600

    [APIs] Make the randomly generated departure time and arrival time more realistic

--- web/app/common/util.py
+++ web/app/common/util.py
@@ -2,6 +2,7 @@
 
 import string
 import random
+import datetime
 from app.models import Ticket
 
 plane_rows = string.ascii_uppercase
@@ -21,3 +22,45 @@ def generate_random_ticket(capacity):
             tickets.append(ticket)
 
     return tickets
+
+
+def generate_random_flight_dates(num_flights):
+    """
+    Randomly generate departing datetime and arriving datetime for flights
+    :param num_flights  Number of flights
+    :return Random flight dates
+    """
+    departure_datetimes = []
+    arrival_datetimes = []
+    flight_duration = [d for d in range(1, 5, 1)] # duration from 1 - 4 days
+    # Mapping from month to the number of days. Ignore leap year for simplicity
+    month_to_date = {1:31, 2:28, 3:31, 4:30, 5:31, 6:30, 7:31, 8:31, 9:30, 10:31, 11:30, 12:31}
+    minutes = [minute for minute in range(0, 60, 5)]
+    now = datetime.datetime.now()
+    for i in range(num_flights):
+        departure_hour = random.randint(1, 23)
+        departure_minute = random.choice(minutes)
+        departure_day = random.randint(now.day, 30)
+        departure_month = random.randint(now.month, 12)
+        departure_year = now.year
+        departure_datetime = datetime.datetime(departure_year, departure_month, departure_day, departure_hour,
+                                               departure_minute, 0)
+
+        arrival_hour = random.randint(1, 23)
+        arrival_minute = random.choice(minutes)
+        arrival_day = departure_day + random.choice(flight_duration)
+
+        if arrival_day <= month_to_date[departure_month]:
+            arrival_month = departure_month
+            arrival_year = departure_year
+        else:
+            arrival_day = arrival_day - month_to_date[departure_month]
+            arrival_month = departure_month + 1 if departure_month < 12 else 1
+            arrival_year = departure_year + 1
+
+        arrival_datetime = datetime.datetime(arrival_year, arrival_month, arrival_day, arrival_hour, arrival_minute, 0)
+
+        departure_datetimes.append(departure_datetime)
+        arrival_datetimes.append(arrival_datetime)
+
+    return departure_datetimes, arrival_datetimes
