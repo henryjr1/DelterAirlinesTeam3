71230ce84db6828cd000afcb7ead6ffdb53aaf77
aqd14 <aqd14@msstate.edu>
2017-12-01 01:28:58 -0600

    Fix bug making tickets unavailable without creating transaction

--- web/app/common/util.py
+++ web/app/common/util.py
@@ -6,7 +6,7 @@ import datetime
 from app.models import Ticket
 
 plane_rows = string.ascii_uppercase
-avaliability = [True, False]
+# avaliability = [True, False]
 prices = [230.0, 135.0, 350.0, 75, 85, 125]
 
 def generate_random_ticket(capacity):
@@ -18,7 +18,7 @@ def generate_random_ticket(capacity):
     rows = int(capacity/col)
     for row in range(rows):
         for col in range(1, col+1):
-            ticket = Ticket(seat_number=plane_rows[row] + str(col), available=random.choice(avaliability), price=random.choice(prices))
+            ticket = Ticket(seat_number=plane_rows[row] + str(col), available=True, price=random.choice(prices))
             tickets.append(ticket)
 
     return tickets
